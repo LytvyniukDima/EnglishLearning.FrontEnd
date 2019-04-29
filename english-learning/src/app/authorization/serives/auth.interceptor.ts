309293474a74d751e3a1a0b85070e3d98ddd5542
dimaLytvyniuk <dima20-12@outlook.com>
2019-04-29 17:52:44 +0300

    Add auth interceptor

---
+++ english-learning/src/app/authorization/serives/auth.interceptor.ts
@@ -0,0 +1,43 @@
+import { Injectable } from '@angular/core';
+import { HttpInterceptor, HttpRequest, HttpHandler, HttpEvent, HttpResponse, HttpErrorResponse } from '@angular/common/http';
+import { Observable, throwError } from 'rxjs';
+import { map, catchError } from 'rxjs/operators';
+import { Router } from '@angular/router';
+
+@Injectable()
+export class AuthInterceptor implements HttpInterceptor {
+    
+    constructor(private router: Router) { }
+
+    intercept(req: HttpRequest<any>, next: HttpHandler){
+        var token = localStorage.getItem('token');
+    
+        if (token)
+            req = req.clone({
+                headers: req.headers.set('Authorization', `Bearer ${token}`)
+        }) 
+
+        return next.handle(req).pipe(
+            map((event: HttpEvent<any>) => {
+                if (event instanceof HttpResponse) {
+                    console.log('event--->>>', event);
+                    // this.errorDialogService.openDialog(event);
+                }
+                return event;
+            }),
+            catchError((error: HttpErrorResponse) => {
+                let data = {};
+                data = {
+                    reason: error && error.error.reason ? error.error.reason : '',
+                    status: error.status
+                };
+
+                if (error.status === 401) {
+                    this.router.navigate(['/sign_in']);
+                    return;
+                }
+
+                return throwError(error);
+            }));
+    }
+}
