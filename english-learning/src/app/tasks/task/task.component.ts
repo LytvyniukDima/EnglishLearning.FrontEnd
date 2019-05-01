16c84a36a6f6dd6d901c62eb699913c12daee530
dimaLytvyniuk <dima20-12@outlook.com>
2019-05-01 11:23:10 +0300

    Add redirection from general task component to concrete

--- english-learning/src/app/tasks/task/task.component.ts
+++ english-learning/src/app/tasks/task/task.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { EnglishTaskModel } from '../models/EnglishTaskModel';
 
 @Component({
   selector: 'app-task',
@@ -6,10 +7,18 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./task.component.css']
 })
 export class TaskComponent implements OnInit {
+  public task = new EnglishTaskModel()
 
   constructor() { }
 
   ngOnInit() {
+    this.task.id = 'asdfasd';
+    this.task.taskType = 'Simple Brackets';
+    this.task.grammarPart = 'Present Simple';
+    this.task.englishLevel = 'Intermediate';
+    this.task.count = 10;
+    this.task.example = '';
+    this.task.text = "A: **I\'ve never been/didn\'t go** to Hollywood.<br>B: Haven\'t you? I **went/\'ve been** there last year.\nA: How many films **have you acted/did you act** in so far?<br>B: I**\'ve acted/acted** in seven films up to now.";
+    this.task.answer = "1\n2";
   }
-
 }
