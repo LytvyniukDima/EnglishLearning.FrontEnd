4f64fe81c065187a1e88917f5cbe5c7ca994094a
dimaLytvyniuk <dima20-12@outlook.com>
2019-05-09 20:22:14 +0300

    Filter videos from api

--- english-learning/src/app/multimedia/services/multimedia.service.ts
+++ english-learning/src/app/multimedia/services/multimedia.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@angular/core';
+import { Injectable, ViewChild, ElementRef } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Router } from '@angular/router';
 import { environment } from './../../../environments/environment';
@@ -12,14 +12,22 @@ export class MultimediaService {
   private readonly videosRandomFullPath: string;
   private readonly videosFullPath: string;
   private readonly videosFiltersPath: string;
+  private readonly videosRandomSearchPath: string;
 
   private readonly apiBaseUrl: string;
+
+  @ViewChild('scroll') private scrollContainer: ElementRef;
+  scrollParent: any;
+
+  scrolledValue = 0;
+  isScrolling = false;
 
   constructor(private http: HttpClient, private router: Router) {
     this.apiBaseUrl = environment['ApiBaseUrl'];
     this.videosRandomFullPath = this.apiBaseUrl.concat('/api/multimedia/random/video');
     this.videosFullPath = this.apiBaseUrl.concat('/api/multimedia/video');
     this.videosFiltersPath = this.apiBaseUrl.concat('/api/multimedia/filters/video/full');
+    this.videosRandomSearchPath = this.apiBaseUrl.concat('/api/multimedia/random/search/video');
   }
 
   getRandomFullVideosList(count = 30) {
@@ -38,20 +46,98 @@ export class MultimediaService {
       return this.http.get<EnglishVideoFullFilterModel>(this.videosFiltersPath);
   }
 
-//   getFilteredRandomInfoTasks(englishLevels: string[], grammarParts: string[], count = 30) {
-//     let uri = this.tasksRandomInfoPath.concat('/' + count.toString() + '/filter?');
+  getFilteredRandomVideos(phrase: string, englishLevels: string[], videoTypes: string[], count = 30) {
+    let uri = this.videosRandomSearchPath.concat('/' + count.toString() + '?');
 
-//     englishLevels.forEach((value) => {
-//         uri = uri.concat('englishLevel=' + value + '&');
-//     })
+    if (phrase && phrase.length > 0) {
+        uri = uri.concat('phrase=' + phrase + '&');
+    }
 
-//     grammarParts.forEach((value) => {
-//       uri = uri.concat('grammarPart=' + value + '&');
-//     })
+    englishLevels.forEach((value) => {
+        uri = uri.concat('englishLevel=' + value + '&');
+    })
 
-//     if (uri.endsWith('&'))
-//         uri = uri.substring(0, uri.length - 1);
+    videoTypes.forEach((value) => {
+      uri = uri.concat('videoType=' + value + '&');
+    })
 
-//     return this.http.get<EnglishTaskInfoModel[]>(uri);
-//   }
+    if (uri.endsWith('&'))
+        uri = uri.substring(0, uri.length - 1);
+
+    return this.http.get<EnglishVideoModel[]>(uri);
+  }
+
+  onScroll() {
+    let element: HTMLElement = document.getElementById('scroll2');
+    console.log(element)
+
+    let scrollFunc = function (node: HTMLElement): HTMLElement {
+      if (node.parentElement == null) {
+        return node;
+      }
+
+      if (node.scrollHeight > node.clientHeight) {
+        return node;
+      } else {
+        return scrollFunc(node.parentElement);
+      }
+    }
+
+    let newScrollParent = scrollFunc(element);
+
+    let scrolledValue = newScrollParent.scrollTop + newScrollParent.clientHeight;
+    let maxScroll = newScrollParent.scrollHeight - 20;
+
+    if (scrolledValue > this.scrolledValue && scrolledValue > maxScroll && !this.isScrolling) {
+      console.log("needScroll");
+      this.isScrolling = true;
+      this.onScrollLoad();
+    }
+    this.scrolledValue = scrolledValue;
+  }
+
+  getScrollParent(node) {
+    if (node.parentNode == null) {
+      return node;
+    }
+
+    if (node.scrollHeight > node.clientHeight) {
+      return node;
+    } else {
+      return this.getScrollParent(node.parentNode);
+    }
+  }
+
+  onScrollLoad() {
+    if (this.englishLevelsFromSearch.length > 0 || this.grammarPartsFromSearch.length > 0) {
+      this.tasksService.getFilteredRandomInfoTasks(this.englishLevelsFromSearch, this.grammarPartsFromSearch).subscribe(data => {
+        this.taskInfoList = this.taskInfoList.concat(data);
+        this.tasksMapper.fixNamingsForInfoModels(this.taskInfoList);
+        this.isScrolling = false;
+      },
+        (err: HttpErrorResponse) => {
+          this.isScrolling = false;
+          if (err.error instanceof Error) {
+            console.log('An error occurred:', err.error.message);
+          } else {
+            console.log(`Backend returned code ${err.status}, body was: ${err.error}`);
+          }
+        })
+    } else {
+      this.tasksService.getRandomTasksInfoList().subscribe(data => {
+        this.taskInfoList = this.taskInfoList.concat(data);
+        console.log(this.taskInfoList);
+        this.tasksMapper.fixNamingsForInfoModels(this.taskInfoList);
+        this.isScrolling = false;
+      },
+        (err: HttpErrorResponse) => {
+          this.isScrolling = false;
+          if (err.error instanceof Error) {
+            console.log('An error occurred:', err.error.message);
+          } else {
+            console.log(`Backend returned code ${err.status}, body was: ${err.error}`);
+          }
+        });
+    }
+  }
 }
--- english-learning/src/app/multimedia/video-list/video-list.component.ts
+++ english-learning/src/app/multimedia/video-list/video-list.component.ts
@@ -15,6 +15,12 @@ export class VideoListComponent implements OnInit {
   public videoFilter: EnglishVideoFullFilterModel;
   public englishLevelsKeys: string[];
   public videoTypesKeys: string[];
+  
+  public textSearch = "";
+  public videoTypesFromSearch: string[];
+  public englishLevelsFromSearch: string[];
+  public videoTypesMap = new Map();
+  public englishLevelsMap = new Map();
 
   constructor(private multimediaService: MultimediaService) { }
 
@@ -60,5 +66,72 @@ export class VideoListComponent implements OnInit {
           console.log(`Backend returned code ${err.status}, body was: ${err.error}`);
         }
       })
+  }
+
+  onChangeSearchField(event) {
+    this.textSearch = event.target.value;
+
+    this.makeSearch();
+  }
+
+  onChagedEnglishLevelBox(event) {
+    let target = event.target;
+    let value = target.value;
+
+    if (target.checked) {
+      this.englishLevelsMap.set(value, true);
+    } else {
+      this.englishLevelsMap.set(value, false);
+    }
+
+    console.log(event);
+  }
+
+  onChangedVideoTypeBox(event) {
+    let target = event.target;
+    let value = target.value;
+
+    if (target.checked) {
+      this.videoTypesMap.set(value, true);
+    } else {
+      this.videoTypesMap.set(value, false);
+    }
+
+    console.log(event);
+  }
+
+  onSearch() {
+    this.englishLevelsFromSearch = [];
+    this.videoTypesFromSearch = [];
+
+    this.englishLevelsMap.forEach((value, key) => {
+      if (value)
+        this.englishLevelsFromSearch.push(key);
+    })
+
+    this.videoTypesMap.forEach((value, key) => {
+      if (value)
+        this.videoTypesFromSearch.push(key);
+    })
+
+    this.makeSearch();
+  }
+
+  makeSearch() {
+    if (this.englishLevelsFromSearch.length > 0 || this.videoTypesFromSearch.length > 0 || this.textSearch.length > 0) {
+      this.multimediaService.getFilteredRandomVideos(this.textSearch, this.englishLevelsFromSearch, this.videoTypesFromSearch).subscribe(data => {
+        console.log(data);
+        this.videoList = data;
+      },
+        (err: HttpErrorResponse) => {
+          if (err.error instanceof Error) {
+            console.log('An error occurred:', err.error.message);
+          } else {
+            console.log(`Backend returned code ${err.status}, body was: ${err.error}`);
+          }
+        })
+    } else {
+      this.getVideos();
+    }
   }
 }
