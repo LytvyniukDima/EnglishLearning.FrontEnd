619b129ddfb3829e808b0f194076c90b85902ec6
dimaLytvyniuk <dima20-12@outlook.com>
2019-05-10 07:02:25 +0300

    Add content for text-list-card

--- english-learning/src/app/multimedia/text-list-card/text-list-card.component.ts
+++ english-learning/src/app/multimedia/text-list-card/text-list-card.component.ts
@@ -1,4 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+import { EnglishTextInfoModel } from '../models/EnglishTextInfoModel';
+import { Router } from '@angular/router';
+import { DomSanitizer } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-text-list-card',
@@ -6,10 +9,16 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./text-list-card.component.css']
 })
 export class TextListCardComponent implements OnInit {
+  @Input() textInfo: EnglishTextInfoModel;
+  public imgUrl;
 
-  constructor() { }
+  constructor(private router: Router, private sanitizer: DomSanitizer) { }
 
   ngOnInit() {
+    if (this.textInfo.englishLevel === 'None')
+      this.textInfo.englishLevel = 'For all levels';
+
+    this.imgUrl = this.sanitizer.bypassSecurityTrustResourceUrl('...');
   }
 
 }
