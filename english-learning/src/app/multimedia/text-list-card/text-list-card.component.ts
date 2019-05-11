2fd01b41f33d0578480340700d6459942839ea3a
dimaLytvyniuk <dima20-12@outlook.com>
2019-05-11 13:26:12 +0300

    add images for text list cards

--- english-learning/src/app/multimedia/text-list-card/text-list-card.component.ts
+++ english-learning/src/app/multimedia/text-list-card/text-list-card.component.ts
@@ -18,10 +18,32 @@ export class TextListCardComponent implements OnInit {
     if (this.textInfo.englishLevel === 'None')
       this.textInfo.englishLevel = 'For all levels';
 
-    this.imgUrl = this.sanitizer.bypassSecurityTrustResourceUrl('...');
+    let img = this.getImagePath();
+    this.imgUrl = this.sanitizer.bypassSecurityTrustResourceUrl(img);
   }
 
   routing() {
     this.router.navigate([`/multimedia/text/${this.textInfo.id}`]);
+  }
+
+  getImagePath(): string {
+    switch(this.textInfo.textType)
+    {
+      case 'Poem': {
+        return '../../../assets/poem.png'
+      }
+      case 'Book Fragment': {
+        return '../../../assets/book.jpg'
+      }
+      case 'Story': {
+        return '../../../assets/story.jpg'
+      }
+      case 'Tragedy fragment': {
+        return '../../../assets/tradegy.jpg'
+      }
+      default: {
+        return '../../../assets/text.jpg'
+      }
+    }
   }
 }
