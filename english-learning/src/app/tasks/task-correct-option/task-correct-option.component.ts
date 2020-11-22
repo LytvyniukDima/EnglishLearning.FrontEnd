9a299b229ed976295ad634ebc7b2ffc3ef511eec
LytvyniukDima <dima.lytvyniuk@gmail.com>
2020-11-22 17:51:03 +0200

    Update core correction option component

--- english-learning/src/app/tasks/task-correct-option/task-correct-option.component.ts
+++ english-learning/src/app/tasks/task-correct-option/task-correct-option.component.ts
@@ -16,9 +16,8 @@ import { CompletedEnglishTaskCreationModel } from '../models/CompletedEnglishTas
 export class TaskCorrectOptionComponent implements OnInit {
   @Input() task: EnglishTaskModel;
 
-  models: EnglishTaskCorrectOptionModel[] = [];
-  usersAnswers: number[];
-  answers: number[];
+  models: EnglishTaskCorrectOptionModel[];
+  userResults: boolean[];
   resultModel = new EnglishTaskResult();
 
   constructor(
@@ -27,53 +26,33 @@ export class TaskCorrectOptionComponent implements OnInit {
     private taskMapper: TasksMapperService) { }
 
   ngOnInit() {
-    this.usersAnswers = new Array(this.models.length);
-    this.answers = new Array(this.models.length);
-    this.parseAnswer();
-    this.parseTask();
+    this.userResults = new Array(this.models.length);
+    this.models = this.parseTask(this.task.content);
   }
 
-  parseTask() {
-    let splitedText = this.task.text.split('\n');
-    for (let i = 0; i < splitedText.length; i++) {
-      let englishTaskCorrectOptionModel = new EnglishTaskCorrectOptionModel();
-      let splitedByContentOption = splitedText[i].split('|');
-      let content = splitedByContentOption[0];
-      let options = splitedByContentOption[1];
+  parseTask(content: string): EnglishTaskCorrectOptionModel[] {
+    const jObject = JSON.parse(content);
 
-      englishTaskCorrectOptionModel.items = content.split('<br>');
-      englishTaskCorrectOptionModel.options = options.split('/');
-
-      englishTaskCorrectOptionModel.answer = this.answers[i];
-      this.models.push(englishTaskCorrectOptionModel);
-    }
-
-    console.log(this.models);
+    return jObject as EnglishTaskCorrectOptionModel[];
   }
 
-  parseAnswer() {
-    let splitedAnswers = this.task.answer.split('\n');
-
-    for (let answer of splitedAnswers) {
-      this.answers.push(parseInt(answer));
-    }
-  }
-
-  onChangedAnswers(answer: number, index: number) {
-    this.usersAnswers[index] = answer;
+  onChangedAnswers(result: boolean, index: number) {
+    this.userResults[index] = result;
   }
 
   onFinish() {
+    this.resultModel = new EnglishTaskResult();
     this.resultModel.correct = 0;
     this.resultModel.incorrect = 0;
 
-    console.log(this.answers);
-    console.log(this.usersAnswers);
-    for (let i = 0; i < this.answers.length; i++) {
-      if (this.answers[i] === this.usersAnswers[i]) {
+    for (let i = 0; i < this.userResults.length; i++) {
+      if (this.userResults[i]) {
         this.resultModel.correct++;
       } else {
         this.resultModel.incorrect++;
+
+        const message = `${i + 1}. correct answer - ${this.models[i].answer}`;
+        this.resultModel.additionalMessages.push(message);
       }
     }
 
