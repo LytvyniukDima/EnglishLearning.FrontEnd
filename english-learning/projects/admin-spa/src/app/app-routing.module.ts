bebd5c4127fe10a257120a6f82a3aba045c28db3
LytvyniukDima <dima.lytvyniuk@gmail.com>
2021-01-05 23:16:41 +0200

    Add sign in page for admin

--- english-learning/projects/admin-spa/src/app/app-routing.module.ts
+++ english-learning/projects/admin-spa/src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { SignInComponent } from 'src/app/authorization/sign-in/sign-in.component';
+import { SignUpComponent } from 'src/app/authorization/sign-up/sign-up.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'sign_in', component: SignInComponent },
+  { path: 'sign_up', component: SignUpComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
