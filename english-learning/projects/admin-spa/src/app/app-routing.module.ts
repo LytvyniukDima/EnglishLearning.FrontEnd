bc9afa90a690e2635929d1f2cda5c800a3e3738e
LytvyniukDima <dima.lytvyniuk@gmail.com>
2021-01-05 23:17:44 +0200

    Remove redundant code

--- english-learning/projects/admin-spa/src/app/app-routing.module.ts
+++ english-learning/projects/admin-spa/src/app/app-routing.module.ts
@@ -1,12 +1,7 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-import { SignInComponent } from 'src/app/authorization/sign-in/sign-in.component';
-import { SignUpComponent } from 'src/app/authorization/sign-up/sign-up.component';
 
-const routes: Routes = [
-  { path: 'sign_in', component: SignInComponent },
-  { path: 'sign_up', component: SignUpComponent }
-];
+const routes: Routes = [];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
