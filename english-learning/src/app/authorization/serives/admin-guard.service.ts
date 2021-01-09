89a8e2b54f5835f26813c88f504c5f533e46b1ae
LytvyniukDima <dima.lytvyniuk@gmail.com>
2021-01-09 12:55:46 +0200

    Add forbidden template

--- english-learning/src/app/authorization/serives/admin-guard.service.ts
+++ english-learning/src/app/authorization/serives/admin-guard.service.ts
@@ -8,6 +8,11 @@ export class AdminGuardService implements CanActivate {
   constructor(public auth: AuthService, public router: Router) {}
   
   canActivate(): boolean {
+    if (!this.auth.isAuthentificated) {
+      this.router.navigate(['sign_in']);
+      return false;
+    }
+
     if (!this.auth.isAdmin) {
       this.router.navigate(['forbidden']);
       return false;
