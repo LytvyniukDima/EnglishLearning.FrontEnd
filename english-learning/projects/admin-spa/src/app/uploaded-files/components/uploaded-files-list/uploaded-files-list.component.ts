b49caa9fb67742da17fb6c3216dca0b0d5bc11cb
LytvyniukDima <dima.lytvyniuk@gmail.com>
2021-01-10 15:50:57 +0200

    feat(admin): add icon in file tree

--- english-learning/projects/admin-spa/src/app/uploaded-files/components/uploaded-files-list/uploaded-files-list.component.ts
+++ english-learning/projects/admin-spa/src/app/uploaded-files/components/uploaded-files-list/uploaded-files-list.component.ts
@@ -16,12 +16,16 @@ export class UploadedFilesListComponent implements OnInit {
 
   @Input() gridItems: GridTreeItemModel[];
 
+  public components = { fileCellRenderer: getFileCellRenderer() };
   public columnDefs = [{ field: 'extension' }, { field: 'lastModified' }];
   public defaultColDef = { flex: 1 };
   public autoGroupColumnDef = {
     headerName: 'Name',
     minWidth: 300,
-    cellRendererParams: { suppressCount: true },
+    cellRendererParams: { 
+      suppressCount: true,
+      innerRenderer: 'fileCellRenderer',
+    },
   };
   public groupDefaultExpanded = -1;
   public getDataPath = function (data: GridTreeItemModel) {
@@ -38,4 +42,34 @@ export class UploadedFilesListComponent implements OnInit {
     this.gridApi = params.api;
     this.gridColumnApi = params.columnApi;
   }
+}
+
+function getFileCellRenderer() {
+  function FileCellRenderer() {}
+  FileCellRenderer.prototype.init = function (params) {
+    var tempDiv = document.createElement('div');
+    var value = params.value;
+    var data = params.data;
+
+    var icon;
+    if (data.isFolder) {
+      icon = 'folder_open';
+    } else {
+      icon = 'text_snippet';
+    }
+
+    tempDiv.innerHTML = icon
+      ? '<span class="filename"><i class="material-icons center-icon">' +
+        icon +
+        '</i>' +
+        '<span></span>' +
+        value +
+        '</span>'
+      : value;
+    this.eGui = tempDiv.firstChild;
+  };
+  FileCellRenderer.prototype.getGui = function () {
+    return this.eGui;
+  };
+  return FileCellRenderer;
 }
