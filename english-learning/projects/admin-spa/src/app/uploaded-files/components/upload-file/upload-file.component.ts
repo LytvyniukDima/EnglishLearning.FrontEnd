272906f40b3afe9d4ce70182bd4c770c638f784b
LytvyniukDima <dima.lytvyniuk@gmail.com>
2021-01-13 22:08:26 +0200

    feat(admin): add metadata section

--- english-learning/projects/admin-spa/src/app/uploaded-files/components/upload-file/upload-file.component.ts
+++ english-learning/projects/admin-spa/src/app/uploaded-files/components/upload-file/upload-file.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { AbstractControl, FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
 import { ActivatedRoute } from '@angular/router';
 
 @Component({
@@ -7,11 +8,75 @@ import { ActivatedRoute } from '@angular/router';
   styleUrls: ['./upload-file.component.scss']
 })
 export class UploadFileComponent implements OnInit {
+  public uploadForm: FormGroup;
 
-  constructor(private route: ActivatedRoute) { }
+  public metadataValuesIndexName = 0;
+  public metadataIndexes: string[] = [];
+
+  constructor(
+    private route: ActivatedRoute,
+    private fb: FormBuilder) { 
+      this.uploadForm = fb.group({});
+  }
 
   ngOnInit(): void {
     const folderId = this.route.snapshot.paramMap.get('id');
     console.log(folderId);
+  }
+
+  onUploadFile() {
+    this.uploadForm.markAllAsTouched();
+
+    if (this.uploadForm.invalid) {
+      return;
+    }
+
+    const metadataMap = [];
+    for(let metadataIndex of this.metadataIndexes) {
+      const key = this.uploadForm.controls[`${metadataIndex}-key`].value;
+      const value = this.uploadForm.controls[`${metadataIndex}-value`].value;
+
+      metadataMap.push({ [key]: value });
+    }
+  }
+
+  onAddMetadata() {
+    const metadataIndex = `metadata-value-${this.metadataValuesIndexName}`;
+    this.metadataIndexes.push(metadataIndex);
+
+    this.uploadForm.addControl(`${metadataIndex}-key`, new FormControl('', [Validators.required, Validators.maxLength(50)]));
+    this.uploadForm.addControl(`${metadataIndex}-value`, new FormControl('', [Validators.required, Validators.maxLength(100)]));
+
+    this.metadataValuesIndexName++;
+  }
+
+  onRemoveMetadata(metadataIndex: string) {
+    const indexOfRemoveItem = this.metadataIndexes.indexOf(metadataIndex);
+    if (indexOfRemoveItem > -1) {
+      this.metadataIndexes.splice(indexOfRemoveItem, 1);
+
+      this.uploadForm.removeControl(`${metadataIndex}-key`);
+      this.uploadForm.removeControl(`${metadataIndex}-value`);
+    }
+  }
+
+  getMetadataKeyIndex(metadataIndex: string): string {
+    return `${metadataIndex}-key`;
+  }
+
+  getMetadataValueIndex(metadataIndex: string): string {
+    return `${metadataIndex}-value`;
+  }
+
+  getFormControl(name: string): AbstractControl {
+    return this.uploadForm.controls[name];
+  }
+
+  getMetadataKeyControl(metadataIndex: string): AbstractControl {
+    return this.getFormControl(this.getMetadataKeyIndex(metadataIndex));
+  }
+
+  getMetadataValueControl(metadataIndex: string): AbstractControl {
+    return this.getFormControl(this.getMetadataValueIndex(metadataIndex));
   }
 }
--- english-learning/projects/admin-spa/src/app/uploaded-files/uploaded-files.module.ts
+++ english-learning/projects/admin-spa/src/app/uploaded-files/uploaded-files.module.ts
@@ -5,13 +5,15 @@ import { UploadedFilesRoutingModule } from './uploaded-files-routing.module';
 import { UploadedFilesListComponent } from './components/uploaded-files-list/uploaded-files-list.component';
 import { AgGridModule } from 'ag-grid-angular';
 import { UploadFileComponent } from './components/upload-file/upload-file.component';
+import { ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [UploadedFilesComponent, UploadedFilesListComponent, UploadFileComponent],
   imports: [
     CommonModule,
     UploadedFilesRoutingModule,
-    AgGridModule.withComponents([])
+    AgGridModule.withComponents([]),
+    ReactiveFormsModule,
   ]
 })
 export class UploadedFilesModule { }
