807930d83ee34a5c9c7202272fa9316e58c57e18
LytvyniukDima <dima.lytvyniuk@gmail.com>
2021-03-08 19:42:17 +0200

    feat(admin): add get all tasks api call

---
+++ english-learning/projects/admin-spa/src/app/tasks/models/task.model.ts
@@ -0,0 +1,9 @@
+export interface TaskModel {
+    id: string;
+    grammarPart: string;
+    taskType: string;
+    englishLevel: string;
+    count: number;
+    example: string;
+    content: string;
+}
--- english-learning/projects/admin-spa/src/app/tasks/services/create-task-api.service.ts
+++ english-learning/projects/admin-spa/src/app/tasks/services/create-task-api.service.ts
@@ -4,18 +4,20 @@ import { Observable } from "rxjs";
 import { environment } from "src/environments/environment";
 import { CreateTaskFromItemsModel } from "../models/create-task-from-items.model";
 import { CreateTaskFromRandomModel } from "../models/create-task-from-random.model";
+import { TaskModel } from "../models/task.model";
 
 @Injectable({
     providedIn: 'root'
 })
 export class CreateTaskApiService {
+    private readonly baseTaskFullPath: string;
     private readonly baseTaskFromItemsPath: string;
-
     private readonly apiBaseUrl: string;
 
     constructor(private httpClient: HttpClient) {
         this.apiBaseUrl = environment['ApiBaseUrl'];
-        this.baseTaskFromItemsPath = this.apiBaseUrl.concat('/api/tasks/full/from-items');
+        this.baseTaskFullPath = this.apiBaseUrl.concat('/api/tasks/full');
+        this.baseTaskFromItemsPath = `${this.baseTaskFullPath}/from-items`;
     }
 
     createFromRandomItems(model: CreateTaskFromRandomModel): Observable<any> {
@@ -26,5 +28,9 @@ export class CreateTaskApiService {
 
     createFromItems(model: CreateTaskFromItemsModel): Observable<any> {
         return this.httpClient.post(this.baseTaskFromItemsPath, model);
+    }
+
+    getAllTasks(): Observable<TaskModel[]> {
+        return this.httpClient.get<TaskModel[]>(this.baseTaskFullPath);
     }
 }
