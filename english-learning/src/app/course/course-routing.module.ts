425bb9c447f878b722bace82322a18b9dc1a37e1
LytvyniukDima <dima.lytvyniuk@gmail.com>
2021-04-01 23:37:44 +0300

    feat: add course list component

--- english-learning/src/app/course/course-routing.module.ts
+++ english-learning/src/app/course/course-routing.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from "@angular/core";
 import { RouterModule, Routes } from "@angular/router";
+import { CourseListContainerComponent } from "./components/course-list-container/course-list-container.component";
 import { ProtoCourseComponent } from "./components/proto-course/proto-course.component";
 
 const routes: Routes = [
     { path: 'course/proto', component: ProtoCourseComponent },
+    { path: 'course/list', component: CourseListContainerComponent },
 ];
 
 @NgModule({
--- english-learning/src/app/course/course.module.ts
+++ english-learning/src/app/course/course.module.ts
@@ -2,11 +2,12 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { CourseRoutingModule } from './course-routing.module';
 import { ProtoCourseComponent } from './components/proto-course/proto-course.component';
+import { CourseListContainerComponent } from './components/course-list-container/course-list-container.component';
 
 
 
 @NgModule({
-  declarations: [ProtoCourseComponent],
+  declarations: [ProtoCourseComponent, CourseListContainerComponent],
   imports: [
     CommonModule,
     CourseRoutingModule,
