5cee4a5b63247bedeb66c7b48e6287dbebf7acd0
LytvyniukDima <dima.lytvyniuk@gmail.com>
2021-04-04 16:46:33 +0300

    feat: sent complete task information

--- english-learning/src/app/course/components/task-train-container/task-train-container.component.ts
+++ english-learning/src/app/course/components/task-train-container/task-train-container.component.ts
@@ -1,9 +1,11 @@
 import { Component, Input, OnInit } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
-import { Observable } from 'rxjs';
+import { ActivatedRoute, Router } from '@angular/router';
+import { forkJoin, Observable } from 'rxjs';
 import { AudioTaskModel } from 'src/app/tasks-core/models/audio-task.model';
 import { CompletedEnglishTaskCreationModel } from 'src/app/tasks-core/models/CompletedEnglishTaskCreationModel';
 import { EnglishTaskModel } from 'src/app/tasks-core/models/EnglishTaskModel';
+import { TasksStatisticService } from 'src/app/tasks/services/tasks-statistic.service';
+import { CompleteTaskCommandModel } from '../../models/complete-task-command.model';
 import { TaskTrainModel } from '../../models/task-train.model';
 import { CourseApiService } from '../../services/course-api.service';
 import { TaskTrainApiService } from '../../services/task-train-api.service';
@@ -24,7 +26,9 @@ export class TaskTrainContainerComponent implements OnInit {
   constructor(
     private route: ActivatedRoute,
     private apiService: CourseApiService,
-    private taskApiService: TaskTrainApiService) { }
+    private taskApiService: TaskTrainApiService,
+    private router: Router,
+    private statisticService: TasksStatisticService) { }
 
   ngOnInit(): void {
     this.grammarPart = this.route.snapshot.paramMap.get('grammarPart');
@@ -40,6 +44,17 @@ export class TaskTrainContainerComponent implements OnInit {
   }
 
   onCompletedRandomTask(completed: CompletedEnglishTaskCreationModel): void {
-    console.log(completed);
+    const taskCompleteModel: CompleteTaskCommandModel = {
+      grammarPart: completed.grammarPart,
+      itemsCount: completed.correctAnswers + completed.incorrectAnswers,
+      correctAnswers: completed.correctAnswers,
+      taskType: this.trainModel.taskType
+    };
+
+    const completeTask$ = this.apiService.completeTask(taskCompleteModel);
+    const statistic$ = this.statisticService.createCompletedEnglishTask(completed);
+    forkJoin([completeTask$, statistic$]).subscribe(_ => {
+      this.router.navigate(['/course/list'])
+    });
   }
 }
